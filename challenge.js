34b483822688d50295864bee93bb26e8e4660139
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-05-22 23:26:55 +0430

    initail solution

--- challenge.js
+++ challenge.js
@@ -1,8 +1,21 @@
 function leftmostNodesSum(array) {
-  // your code here
+  let result = 0;
+  let leftNode = 0;
+  while (true) {
+    if (leftNode >= array.length) {
+      break;
+    }else{
+      result += array[leftNode];
+      leftNode = 2 * leftNode + 1;
+      // console.log(result);
+      // console.log(leftNode);
+    }
+    
+  }
+  return result;
 }
 
-console.log(leftmostNodesSum([2, 7, 5, 2, 6, 0, 9]))
+console.log(leftmostNodesSum([5, 3, 4, 11, 13, 15, 21, 10, 4, 7, 2, 8, 0, 9, 0]))
 // => 11
 
 module.exports = leftmostNodesSum
